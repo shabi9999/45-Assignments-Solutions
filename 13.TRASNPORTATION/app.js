3cd556ea4afce4e89ec55da5f573017a5171885e
shabi9999 <163296693+shabi9999@users.noreply.github.com>
2024-04-01 03:41:23 +0500

    Add files via upload

---
+++ 13.TRASNPORTATION/app.js
@@ -0,0 +1,6 @@
+// exercise 13
+var transportation = ['civic', 'bike', 'bus', 'suzuki jisme cow ho'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log('i would like to own a ' + transportation[i]);
+}
+;
--- 13.TRASNPORTATION/app.ts
+++ 13.TRASNPORTATION/app.ts
@@ -1,1 +1,3 @@
-
+// exercise 13
+let transportation : string [] = ['civic','bike','bus','suzuki jisme cow ho'];
+for(let i=0; i<transportation.length; i++){console.log('i would like to own a '+ transportation[i])};
