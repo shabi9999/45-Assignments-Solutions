e240baaec3d71b44d16034133af251021e75632e
shabi9999 <163296693+shabi9999@users.noreply.github.com>
2024-04-01 03:39:06 +0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// EXERCISE 12
-var members = ["SHAHBAZ", "MJSTAFA", "MUNEEB", "MOMI", "YASIR", "IBAD", "HAMZA"];
-var message = "Mere konsay dost ki shadi hogyi hy  =  ";
-for (var i = 0; i < members.length; i++) {
-    console.log(message + members[i]);
-}
