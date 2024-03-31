8a9627a36aa0cde7276fc4456c211863558275d1
shabi9999 <163296693+shabi9999@users.noreply.github.com>
2024-04-01 03:37:03 +0500

    Add files via upload

--- 12.Greetings/app.ts
+++ 12.Greetings/app.ts
@@ -1,1 +1,6 @@
-
+// EXERCISE 12
+let members: string[] =[`SHAHBAZ`,`MJSTAFA`,`MUNEEB`,`MOMI`,`YASIR`,`IBAD`,`HAMZA`];
+let message: string=`Mere konsay dost ki shadi hogyi hy  =  `;
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i])
+}
