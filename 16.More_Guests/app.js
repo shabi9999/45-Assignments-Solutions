c1408d6a78e03920e7bd06ff507f2f13b059ec9b
shabi9999 <163296693+shabi9999@users.noreply.github.com>
2024-04-01 03:52:42 +0500

    Add files via upload

---
+++ 16.More_Guests/app.js
@@ -0,0 +1,19 @@
+var guest_list = ["MOMI", "YASIR", "MUNEEB", "MURTAZA", "MUSTAFA"];
+// for(let i=0; i<guest_list.length; i++){console.log('Respected Sir/Madam'  + guest_list[i] +
+// ',\n\nWe invited you on dinner tommorow.\nThank You\n')}
+//  guest_list.forEach(guest_list => {console.log(`Dear ${guest_list}, Would you like to join me for diner?` );});
+var not_present = "YASIR";
+var new_guest = "BABAR AZAM";
+guest_list[1] = new_guest;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Respected Sir/Madam " +
+        guest_list[i] +
+        ",\n\nWe invited you on dinner tommorow.\nThank You\n");
+}
+console.log("Mr. ".concat(not_present, " will not coming tomorrow for dinner."));
+guest_list.unshift("SHAHEEN", "SARFARAZ", "RIZWAN");
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Respected Sir/Madam " +
+        guest_list[i] +
+        ",\n\nWe invited you on dinner tommorow. we found a big table so we decide to invite 3 more guests.\nThank You\n");
+}
--- 16.More_Guests/app.ts
+++ 16.More_Guests/app.ts
@@ -1,1 +1,24 @@
+let guest_list: string[] = ["MOMI", "YASIR", "MUNEEB", "MURTAZA", "MUSTAFA"];
+// for(let i=0; i<guest_list.length; i++){console.log('Respected Sir/Madam'  + guest_list[i] +
+// ',\n\nWe invited you on dinner tommorow.\nThank You\n')}
+//  guest_list.forEach(guest_list => {console.log(`Dear ${guest_list}, Would you like to join me for diner?` );});
+let not_present: string = "YASIR";
+let new_guest: string = "BABAR AZAM";
+guest_list[1] = new_guest;
+for (let i = 0; i < guest_list.length; i++) {
+  console.log(
+    "Respected Sir/Madam " +
+      guest_list[i] +
+      ",\n\nWe invited you on dinner tommorow.\nThank You\n"
+  );
+}
+console.log(`Mr. ${not_present} will not coming tomorrow for dinner.`);
 
+guest_list.unshift("SHAHEEN", "SARFARAZ", "RIZWAN");
+for (let i = 0; i < guest_list.length; i++) {
+  console.log(
+    "Respected Sir/Madam " +
+      guest_list[i] +
+      ",\n\nWe invited you on dinner tommorow. we found a big table so we decide to invite 3 more guests.\nThank You\n"
+  );
+}
