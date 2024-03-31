97181c207cdd843cefbd8b08081f7b055ba2667c
shabi9999 <163296693+shabi9999@users.noreply.github.com>
2024-04-01 03:32:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// EXERCISE 12
+var members = ["SHAHBAZ", "MJSTAFA", "MUNEEB", "MOMI", "YASIR", "IBAD", "HAMZA"];
+var message = "Mere konsay dost ki shadi hogyi hy  =  ";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
