f6b8274157b1f075f76c87f2c092304d92ff66c0
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-03 07:02:48 +0500

    Add files via upload

---
+++ 19,DINNER_GUESTS/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var appguests_1 = require("../14.GUEST_LIST/appguests");
+console.log("n PRINTING NUMBER OF GUEST INVITED");
+console.log("we had finally invited ".concat(appguests_1.guest_list.length, " from exercise 14"));
--- 19,DINNER_GUESTS/app.ts
+++ 19,DINNER_GUESTS/app.ts
@@ -1,1 +1,3 @@
-
+import{ guest_list } from'../14.GUEST_LIST/appguests'
+console.log(`n\ PRINTING NUMBER OF GUEST INVITED`) 
+console.log(`we had finally invited ${guest_list.length} from exercise 14`)
