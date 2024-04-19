a914609f9dbf922afbb89ba37d491c06820cde12
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-19 23:59:34 +0500

    Add files via upload

---
+++ 21.TYPESCRIPT_OBJECT/app.js
@@ -0,0 +1,11 @@
+// create two objecs
+var book = {
+    name: 'ESSENTIAL TYPESCRIPT',
+    price: 450
+};
+var apple = {
+    name: 'apple',
+    price: 200
+};
+console.log("book name: ".concat(book.name, ",\nprice: $").concat(book.price));
+console.log("apple name: ".concat(apple.name, ",\nprice: $").concat(apple.price));
--- 21.TYPESCRIPT_OBJECT/app.ts
+++ 21.TYPESCRIPT_OBJECT/app.ts
@@ -1,1 +1,15 @@
-
+interface item {
+    name:string
+    price:number
+}
+// create two objecs
+const book: item ={
+    name: 'ESSENTIAL TYPESCRIPT',
+    price:450
+}
+const apple: item = {
+    name:'apple',
+    price:200
+}
+console.log(`book name: ${book.name},\nprice: $${book.price}`)
+console.log(`apple name: ${apple.name},\nprice: $${apple.price}`)
