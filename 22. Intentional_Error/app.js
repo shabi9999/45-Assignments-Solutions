0d53ea9a643f7a232852ae5b15df592769348e03
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-20 00:20:58 +0500

    Add files via upload

---
+++ 22. Intentional_Error/app.js
@@ -0,0 +1,5 @@
+"use strict";
+//  Intentional Error
+let array = ['shahbaz', 'momi', 'yasir', 6, 7, 29];
+console.log(array[6]); // Intentional error: Arrays are zero-indexed, so index 5 is out of bounds.
+console.log(array[0]); // Correcting the error by accessing a valid index.tsc
--- 22. Intentional_Error/app.ts
+++ 22. Intentional_Error/app.ts
@@ -1,1 +1,4 @@
-
+//  Intentional Error
+let array: (string | number) [] =['shahbaz','momi','yasir',6,7,29]
+console.log(array[6]); // Intentional error: Arrays are zero-indexed, so index 5 is out of bounds.
+console.log(array[0]); // Correcting the error by accessing a valid index.tsc
