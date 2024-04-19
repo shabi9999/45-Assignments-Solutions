1d168c46d9bb553af3a2da11fbc4e45b53f9c332
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-20 01:03:08 +0500

    Add files via upload

--- 23.Conditional_Tests/app.ts
+++ 23.Conditional_Tests/app.ts
@@ -1,1 +1,59 @@
+let car: string = 'subaru'
+// Test 1: Equality comparision (True)
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car == 'subaru'); // True
+
+
+
+// Test 2: Equality comparision (True)
+console.log("Is car === 'subaru' ? I predict True.");
+console.log(car === 'subaru'); // True
+
+
+
+
+// Test 3: Inequality comparision (False)
+console.log("Is car != 'subaru' ? I predict False.");
+console.log(car != 'subaru'); // False
+
+
+// Test 4: Strict Inequality comparision (False)
+console.log("Is car !== 'subaru' ? I predict False.");
+console.log(car !== 'subaru'); // False
+
+
+
+// Test 5: Less than comparision (False)
+console.log("Is car < 'subaru' ? I predict False.");
+console.log(car < 'subaru'); // False
+
+
+
+// Test 6: Greater than comparision (False)
+console.log("Is car > 'subaru' ? I predict False.");
+console.log(car > 'subaru'); // False
+
+
+// Test 7: Less than or Equal comparision (True)
+console.log("Is car <= 'subaru' ? I predict True.");
+console.log(car <= 'subaru'); // True
+
+// Test 8: Greater than or Equal comparision (True)
+console.log("Is car >= 'subaru' ? I predict True.");
+console.log(car >= 'subaru'); // True
+
+
+
+// Test 9: Cheacking truthness(True)
+console.log("Is car? I predict True.");
+console.log(car); // True (non-empty string is truthy)
+
+
+
+// Test 10: Cheacking Falsiness (False)
+console.log("Is !car? I predict False.");
+console.log(!car); // False (negation of a truthy value)
+
+
+
 
