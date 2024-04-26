86fddde303369b785b3caf7ead6164d1198b51b5
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-26 23:26:38 +0500

    Add files via upload

---
+++ 26.ALIEN_COLOUR_2/app.js
@@ -0,0 +1,12 @@
+"use strict";
+let alien_colour = "red";
+if (alien_colour == "green") {
+    console.log("the player just earn 5 points for shooting the alien.");
+}
+else {
+    console.log("the player just earn 10 points.");
+}
+alien_colour = "green";
+if (alien_colour == "red") {
+    console.log("the player just earn 5 points for shooting the alien.");
+}
--- 26.ALIEN_COLOUR_2/app.ts
+++ 26.ALIEN_COLOUR_2/app.ts
@@ -1,1 +1,12 @@
+let alien_colour: string = "red";
 
+if(alien_colour == "red"){
+    console.log("the player just earn 5 points for shooting the alien.");
+}
+else{console.log("the player just earn 10 points.")
+
+}
+alien_colour = "green";
+if(alien_colour == "red"){
+    console.log("the player just earn 5 points for shooting the alien.")
+}
