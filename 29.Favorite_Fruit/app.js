474cf847e06796d307c6fb7f1117c9b324b62e98
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-27 00:45:09 +0500

    Add files via upload

---
+++ 29.Favorite_Fruit/app.js
@@ -0,0 +1,17 @@
+"use strict";
+let favorite_Fruits = ['Banana', 'Grapes', 'Mango'];
+if (favorite_Fruits.includes('Banana')) {
+    console.log("i really like Bananas!");
+}
+if (favorite_Fruits.includes('Grapes')) {
+    console.log("i really like Grapes!");
+}
+if (favorite_Fruits.includes('Mango')) {
+    console.log("i really like Mango!");
+}
+if (favorite_Fruits.includes('apple')) {
+    console.log("i really like apples!");
+}
+if (favorite_Fruits.includes('watermelon')) {
+    console.log("i really like watermelons!");
+}
--- 29.Favorite_Fruit/app.ts
+++ 29.Favorite_Fruit/app.ts
@@ -1,1 +1,16 @@
-
+let favorite_Fruits : string [] =['Banana','Grapes','Mango']
+if(favorite_Fruits.includes('Banana')){
+    console.log("i really like Bananas!")
+}
+if(favorite_Fruits.includes('Grapes')){
+    console.log("i really like Grapes!")
+}
+if(favorite_Fruits.includes('Mango')){
+    console.log("i really like Mango!")
+}
+if(favorite_Fruits.includes('apple')){
+    console.log("i really like apples!")
+}
+if(favorite_Fruits.includes('watermelon')){
+    console.log("i really like watermelons!")
+}
