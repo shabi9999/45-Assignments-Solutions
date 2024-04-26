0f237300684a206c176283d0178dd874b07a070e
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-26 23:56:45 +0500

    Add files via upload

---
+++ 27.ALIEN_COLOUR_3/app.js
@@ -0,0 +1,42 @@
+"use strict";
+let alien_colour = "green";
+if (alien_colour == "green") {
+    console.log("the player just earned 5 points.");
+}
+else if (alien_colour == "yellow") {
+    console.log("the player just earned 10 points.");
+}
+else if (alien_colour == "red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("Please select right colour");
+}
+// version 2
+alien_colour = "yellow";
+if (alien_colour == "green") {
+    console.log("the player just earned 5 points.");
+}
+else if (alien_colour == "yellow") {
+    console.log("the player just earned 10 points.");
+}
+else if (alien_colour == "red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("Please select right colour");
+}
+// version 3
+alien_colour = "red";
+if (alien_colour == "green") {
+    console.log("the player just earned 5 points.");
+}
+else if (alien_colour == "yellow") {
+    console.log("the player just earned 10 points.");
+}
+else if (alien_colour == "red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("Please select right colour");
+}
--- 27.ALIEN_COLOUR_3/app.ts
+++ 27.ALIEN_COLOUR_3/app.ts
@@ -1,1 +1,37 @@
+let alien_colour: string = "green";
+// version 1
+if(alien_colour == "green"){
+    console.log("the player just earned 5 points.");
+}else if(alien_colour == "yellow"){
+    console.log("the player just earned 10 points.");
+}else if (alien_colour == "red") {
+    console.log("the player just earned 15 points.");
+}else{console.log("Please select right colour")
 
+}
+
+// version 2
+alien_colour = "yellow";
+
+if(alien_colour == "green"){
+    console.log("the player just earned 5 points.");
+}else if(alien_colour == "yellow"){
+    console.log("the player just earned 10 points.");
+}else if (alien_colour == "red") {
+    console.log("the player just earned 15 points.");
+}else{console.log("Please select right colour")
+
+}
+
+// version 3
+ alien_colour = "red";
+
+if(alien_colour == "green"){
+    console.log("the player just earned 5 points.");
+}else if(alien_colour == "yellow"){
+    console.log("the player just earned 10 points.");
+}else if (alien_colour == "red") {
+    console.log("the player just earned 15 points.");
+}else{console.log("Please select right colour")
+
+}
