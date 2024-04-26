79aa038a34aad21c5be4892a2c3f9d6544826fcd
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-26 23:07:55 +0500

    Add files via upload

---
+++ 25.ALIEN_COLOUR_1/app.js
@@ -0,0 +1,11 @@
+"use strict";
+// create a variable called alien_colour
+let alien_colour = "red";
+if (alien_colour == "red") {
+    console.log("the player just earn 5 points.");
+}
+// The version that fail will have no output.
+alien_colour = "green";
+if (alien_colour == "red") {
+    console.log("the player just earn 5 points.");
+}
--- 25.ALIEN_COLOUR_1/app.ts
+++ 25.ALIEN_COLOUR_1/app.ts
@@ -1,1 +1,13 @@
+// create a variable called alien_colour
+let alien_colour: string = "red";
 
+if(alien_colour == "red"){
+    console.log("the player just earn 5 points.");
+}
+
+// The version that fail will have no output.
+
+alien_colour = "green"
+if(alien_colour == "red"){
+    console.log("the player just earn 5 points.");
+}
