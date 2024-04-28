6c5840fab010b8fb2b70292b1f8af4030b3e1e63
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-29 01:03:19 +0500

    Add files via upload

---
+++ 32.Checking_Usernames/app.js
@@ -0,0 +1,11 @@
+"use strict";
+let current_users = ["shahbaz", "admin", "momi", "muneeb", "yasir"];
+let new_users = ["shahbaz", "mustafa", "murtaza", "user7", "admin", "user9"];
+new_users.forEach((newUser) => {
+    if (current_users.some((current_Users) => current_Users.toLowerCase() === newUser.toLowerCase())) {
+        console.log(`${newUser} will need to enter a new username.`);
+    }
+    else {
+        console.log(`${newUser} is available.`);
+    }
+});
--- 32.Checking_Usernames/app.ts
+++ 32.Checking_Usernames/app.ts
@@ -1,1 +1,12 @@
+let current_users: string[] =["shahbaz","admin","momi","muneeb","yasir"];
+let new_users: string[] =["shahbaz","mustafa","murtaza","user7","admin","user9"];
 
+new_users.forEach((newUser) => {
+    if(current_users.some(
+       (current_Users) => current_Users.toLowerCase() === newUser.toLowerCase()
+    )){
+        console.log(`${newUser} will need to enter a new username.`);
+    }else{
+        console.log(`${newUser} is available.`)
+    }
+})
