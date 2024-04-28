26259fdb8e4d4082cfd7b2b9dc5d47d5e02c2848
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-04-29 00:33:38 +0500

    Add files via upload

---
+++ 31.No_Users/app.js
@@ -0,0 +1,9 @@
+"use strict";
+let users = ['shahbaz', 'momi', 'yasir', 'muneeb', 'mustafa'];
+if (users.length === 0) {
+    console.log("we need to fine some users! ");
+}
+else {
+    users = [];
+    console.log("ALL user have been removed " + users.length);
+}
--- 31.No_Users/app.ts
+++ 31.No_Users/app.ts
@@ -1,1 +1,7 @@
-
+let users : string [] = ['shahbaz','momi','yasir','muneeb','mustafa']
+if(users.length === 0){
+    console.log("we need to fine some users! ")
+}
+else{users = [] ;
+    console.log("ALL user have been removed " + users.length)
+}
