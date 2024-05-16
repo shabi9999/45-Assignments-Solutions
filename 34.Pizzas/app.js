9455547d5e2a899a86851bc0a43fa6b4263d5c93
SHAIKH SHAHBAZ <163296693+shabi9999@users.noreply.github.com>
2024-05-17 00:28:29 +0500

    Add files via upload

---
+++ 34.Pizzas/app.js
@@ -0,0 +1,10 @@
+"use strict";
+let myPizza = ["BBQ PIZZA", "FAJITA PIZZA", "AFGHANI PIZZA",];
+for (let i = 0; i < myPizza.length; i++) {
+    console.log(myPizza[i]);
+}
+for (let i = 0; i < myPizza.length; i++) {
+    console.log(`i like to eat  ${myPizza[i]}`);
+}
+console.log(`]nI really like to eat Pizzas. Pizza comes in a variety of flavors and toppings,allowing indiividual customize it to their liking
+`);
--- 34.Pizzas/app.ts
+++ 34.Pizzas/app.ts
@@ -1,1 +1,10 @@
-
+// exercise 34
+let myPizza = ["BBQ PIZZA","FAJITA PIZZA","AFGHANI PIZZA",]
+for ( let i = 0; i <myPizza.length; i++) {
+    console.log(myPizza[i]);
+}
+for ( let i = 0; i <myPizza.length; i++) {
+    console.log(`i like to eat ${myPizza[i]}`);
+}
+console.log(`]n I really like to eat Pizzas. Pizza comes in a variety of flavors and toppings,allowing indiividual customize it to their liking
+`);
